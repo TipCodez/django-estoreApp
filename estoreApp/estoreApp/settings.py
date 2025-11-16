31f4a501a26c43f85a9e96aa6e2642e0540f128f
Punobyin Raphael Tibil <tipraph@gmail.com>
2025-11-15 22:20:24 -0800

    Comfiguration of Postgres and Security settings

--- estoreApp/estoreApp/settings.py
+++ estoreApp/estoreApp/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os 
 import environ
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Initialize environment variables
 env = environ.Env(
@@ -83,12 +86,25 @@ WSGI_APPLICATION = 'estoreApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
+
+
 
 
 # Password validation
