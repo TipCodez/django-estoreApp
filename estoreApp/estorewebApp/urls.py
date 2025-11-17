5caa53294d6d81becf53750641479aa28e9beac4
Punobyin Raphael Tibil <tipraph@gmail.com>
2025-11-16 22:55:59 -0800

    mapping signup, login, logouy urls

--- estoreApp/estorewebApp/urls.py
+++ estoreApp/estorewebApp/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('product/<int:product_id>/', views.product, name='product_detail'),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- estoreApp/estorewebApp/views.py
+++ estoreApp/estorewebApp/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from .models import Profile, Product, Order, OrderItem
+
 
 
 def home(request):
@@ -64,7 +65,23 @@ def signup_view(request):
         login(request, user)
         return redirect("home")
 
-    return render(request, "accounts/signup.html")
+    return render(request, "signup.html")
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password.")
+            return redirect("login")
+
+    return render(request, "login.html")
 
 
 def contact(request):
